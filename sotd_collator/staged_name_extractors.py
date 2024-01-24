f1376a8300e4fec4fc872a865d7d774f98548a3e
Jim <jim@jmoore.org>
2024-01-24 16:10:18 -0600

    StagedBladeUseExtractor

--- sotd_collator/staged_name_extractors.py
+++ sotd_collator/staged_name_extractors.py
@@ -1,7 +1,5 @@
 import re
-from functools import cached_property
-from sotd_collator.base_alternate_namer import BaseAlternateNamer
-from sotd_collator.razor_alternate_namer import RazorAlternateNamer
+
 from sotd_collator.base_name_extractor import BaseNameExtractor
 
 
@@ -22,4 +20,22 @@ class StagedBrushNameExtractor(BaseNameExtractor):
     @BaseNameExtractor.post_process_name
     def get_name(self, comment):
         return comment["brush"] if "brush" in comment else None
+
+class StagedBladeUseExtractor(BaseNameExtractor):
+
+    def extract_blade_use(self, input_string):
+        pattern = r'\((\d+)\)|\[(\d+)\]|\{(\d+)\}'
+        match = re.search(pattern, input_string)
+        
+        if match:
+            return int(match.group(1) or match.group(2) or match.group(3))
+        else:
+            return None
+    
+    def get_name(self, comment):
+        if "blade" not in comment: return None
+        return self.extract_blade_use(comment["blade"])
+        
+
+
 
